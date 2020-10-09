bb21f930f94aa309ac1fcfa44d60cd8e6fe15f89
Muhammad Fauzi <muhammadfauzi.2.mf52@gmail.com>
2020-10-09 16:34:41 +0700

    menambahkan class component dan const variable

--- App.js
+++ App.js
@@ -1,89 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import {Text, View, Image, TextInput} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-              <Text>Hello World</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: '#12CBC4'}}/>
+      <Fauzi />
+        <Text>Hello World</Text>
+        <Text>Hello World</Text>
+        <Text>Hello World</Text>
+        <Gambar />
+        <TextInput style={{borderWidth: 1}}></TextInput>
+        <Aku />
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const Fauzi = () => {
+  return<Text>Muhammad Fauzi</Text>;
+};
+
+const Gambar = () => {
+  return <Image source={{ uri:'https://reactnative.dev/img/tiny_logo.png' }} 
+    style={{ width: 100, height: 100}}/>;
+    
+};
+
+class Aku extends Component {
+  render() {
+  const name = "bapak samran dan ibu agustinah"
+  return <Text>ayah dan ibuku, { name }</Text>
+  
+  }
+}
+
 
 export default App;
